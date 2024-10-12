d6a8b850e635fd4d0f051a3355e021fb7deaf568
VincentMugondora <vinmugondora@gmail.com>
2024-10-12 21:29:53 +0200

    updated

--- src/Components/MainAbout.jsx
+++ src/Components/MainAbout.jsx
@@ -1,73 +1,29 @@
-import React from 'react';
+
 
 
 const MainAbout = () => {
   return (
-    <section id="about" className="about container">
-      <div className="oval">
-        <div className="container-1">
-          <div className="row">
-            <div className="col-md-8 left">
-              <div className="main-text">
-                <h2>About me</h2>
-                <span>
-                  I'm Tendai Chakawuya, a passionate entrepreneur, software
-                  developer, and business owner who believes in harnessing the
-                  power of technology to drive positive change in the world.
-                  With a passion for innovation and a drive to make a
-                  difference, I'm dedicated to creating solutions that impact
-                  lives and shape the future. When I'm not coding or building
-                  businesses, you can find me in the fields, exploring my
-                  passion for agriculture and sustainable living. I believe in
-                  the importance of feeding our communities and promoting
-                  eco-friendly practices. As a car enthusiast, I also enjoy the
-                  thrill of the open road and the beauty of well-designed
-                  machines.
-                </span>
-              </div>
-            </div>
-            <div className="col-md-4 right yy about-img yyy">
-              <div className="imgg">
-                <img src="/src/assets/img/home/about-pic.jpg" alt="img" />
-              </div>
-              <div className="imggg">
-                <img src="/src/assets/img/home/about-pic-2.jpg" alt="img" />
-              </div>
-            </div>
+    <section id="about" className="about-one container">
+      <div className="row">
+        <div className="col-md-8">
+          <div className="main-text">
+            <h2 className="heading">About me</h2>
+            <p>
+              I am Tendai Chakawuya, a passionate entrepreneur, software
+              developer, and business owner who believes in harnessing the power
+              of technology to drive positive change in the world. With a
+              passion for innovation and a drive to make a difference, I am
+              dedicated to creating solutions that impact lives and shape the
+              future. When I am not coding or building businesses, you can find
+              me in the fields, exploring my passion for agriculture and
+              sustainable living. I believe in the importance of feeding our
+              communities and promoting eco-friendly practices. As a car
+              enthusiast, I also enjoy the thrill of the open road and the
+              beauty of well-designed machines.
+            </p>
           </div>
         </div>
-      </div>
-      <div className="oval">
-        <div className="container-1">
-          <div className="row">
-            <div className="col-md-8 left">
-              <div className="main-text">
-                <h2>Journey</h2>
-                <span>
-                  I set out on a challenge to build a series companies,
-                  each one a stepping stone to greater success. From my first
-                  venture, a software development company, to my latest project,
-                  a car customizing commpany, each company has taught me
-                  valuable lessons and fueled my passion for innovation. Along
-                  the way, I've faced challenges, embraced failures, and
-                  celebrated triumphs. But most importantly, I've remained true
-                  to my vision of creating a business empire that makes a
-                  lasting impact. As I continue on this road to 21, I'm excited
-                  to explore new industries, push boundaries, and inspire others
-                  to join me on this entrepreneurial journey.
-                </span>
-              </div>
-            </div>
-            <div className="col-md-4 right about-img yyy y">
-              <div className="imgg">
-                <img src="/src/assets/img/home/journey-pic.jpg" alt="img" />
-              </div>
-              <div className="imggg">
-                <img src="/src/assets/img/home/about-pic-2.jpg" alt="img" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="col-md-4"></div>
       </div>
     </section>
   );
