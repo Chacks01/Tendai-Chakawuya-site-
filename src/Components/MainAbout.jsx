ea3964c852ec928ba6a044a4ce360b6fd951978e
VincentMugondora <vinmugondora@gmail.com>
2024-10-14 13:44:52 +0200

    updated main

--- src/Components/MainAbout.jsx
+++ src/Components/MainAbout.jsx
@@ -11,12 +11,12 @@ const MainAbout = () => {
               <div className="main-text">
                 <h2>About me</h2>
                 <span>
-                  I'm Tendai Chakawuya, a passionate entrepreneur, software
+                  I am Tendai Chakawuya, a passionate entrepreneur, software
                   developer, and business owner who believes in harnessing the
                   power of technology to drive positive change in the world.
                   With a passion for innovation and a drive to make a
-                  difference, I'm dedicated to creating solutions that impact
-                  lives and shape the future. When I'm not coding or building
+                  difference, I am dedicated to creating solutions that impact
+                  lives and shape the future. When I am not coding or building
                   businesses, you can find me in the fields, exploring my
                   passion for agriculture and sustainable living. I believe in
                   the importance of feeding our communities and promoting
